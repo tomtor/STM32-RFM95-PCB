06176a1310023f0a6797c8b4f2ed035f31d960bc
Tom Vijlbrief <tvijlbrief@gmail.com>
2025-03-21 14:26:39 +0100

    Add RadioLib LORA example for AVR128

---
+++ AVR128_RadioLib/AVR128_RadioLib.ino
@@ -0,0 +1,310 @@
+/*
+ * Adapted RadioLib example for AVR128DB with rain bucket counter
+ */
+
+#include "config.h"
+
+#include <avr/sleep.h>
+
+int uplinkIntervalSeconds = 1 * 60;    // minutes x seconds
+
+#define LED             PIN_PA7
+#define COUNT_PIN       PIN_PA2
+
+#define ON_TIME         3 // LED ON blink time in ms
+
+#define RTC_PERIOD	    0x8000 // Overflow at 32768 ticks from RTC
+#define RTC_MILLIS	    1000   // That matches 1000ms
+
+volatile unsigned counter;  // Counts pin transitions to low
+
+typedef uint8_t u1_t;
+
+void fillDEVEUI (u1_t* buf) { // Fetch AVR128 serial numbers
+  // use chip ID:
+  u1_t *serial= (u1_t*)&SIGROW.SERNUM0;
+  for (byte i= 0; i <= 7; i++)
+    buf[i] = serial[i] ^ serial[8+i];
+  // Make locally registered:
+  buf[0] = (buf[0] & ~0x3) | 0x1;
+
+  Serial.print("DevEui: ");
+  for (int i = 7; i >= 0; i--) {
+    Serial.printHex((uint8_t)buf[i]);
+    Serial.print(' ');
+  }
+  Serial.println("");
+}
+
+void RTC_init(void)
+{
+  /* Initialize RTC: */
+  while (RTC.STATUS > 0)
+    ;                                   /* Wait for all register to be synchronized */
+  RTC.PER = RTC_PERIOD - 1;		/* 1 sec overflow */
+  RTC.CLKSEL = RTC_CLKSEL_OSC32K_gc;    /* 32.768kHz Internal Ultra-Low-Power Oscillator (OSCULP32K) */
+  //RTC.PITINTCTRL = RTC_PI_bm;           /* PIT Interrupt: enabled */
+
+  // For overflow ticks while sleeping:
+  RTC.INTCTRL = RTC_OVF_bm;
+
+  RTC.CTRLA |= RTC_PRESCALER_DIV1_gc | RTC_RUNSTDBY_bm | RTC_RTCEN_bm;
+}
+
+volatile uint8_t sleep_cnt;
+
+volatile uint8_t secs, hours, minutes;
+volatile uint16_t time_s;
+
+ISR(RTC_CNT_vect)
+{
+  if (RTC.INTFLAGS & RTC_CMP_bm) {
+    sleep_cnt--;
+    RTC.INTFLAGS = RTC_CMP_bm;            /* Clear interrupt flag by writing '1' (required) */
+  }
+
+  if (RTC.INTFLAGS & RTC_OVF_bm) {
+    time_s++;
+    if (secs < 59)
+      secs += 1;
+    else {
+      secs = 0;
+      if (minutes < 59)
+        minutes++;
+      else {
+        minutes = 0;
+        if (hours < 23)
+          hours++;
+        else
+          hours = 0;
+      }  
+    }
+    RTC.INTFLAGS = RTC_OVF_bm;          /* Clear interrupt flag by writing '1' (required) */
+  }
+}
+
+bool idle_mode = true;
+
+void sleep_idle() // We do not use real idle mode, but just keep TCB2 running in standby
+{
+  idle_mode = true;
+  TCB2.CTRLA = TCB2.CTRLA | TCB_RUNSTDBY_bm;
+}
+
+void sleep_standby()
+{
+  idle_mode = false;
+  TCB2.CTRLA = TCB2.CTRLA & ~TCB_RUNSTDBY_bm;
+}
+
+void sleepDelay(uint16_t orgn, bool precise=false)
+{
+  while (RTC.STATUS /* & RTC_CMPBUSY_bm */)  // Wait for new settings to synchronize
+    ;
+
+  uint32_t delay;
+  RTC.CMP = (RTC.CNT + (delay = (orgn * 32UL) + uint16_t(orgn / 128 * 3))) & (RTC_PERIOD-1); // With this calculation every multiple of 128ms is exact!
+
+  while (RTC.STATUS /* & RTC_CMPBUSY_bm */)  // Wait for new settings to synchronize
+    ;
+  
+  RTC.INTCTRL |= RTC_CMP_bm; // This might trigger a pending interrupt, so do this before assigning sleep_cnt!
+  sleep_cnt = delay / RTC_PERIOD + 1; // Calculate number of wrap arounds (overflows)
+  uint64_t start = millis();
+  while (sleep_cnt) {
+    sleep_cpu();
+  }
+  if (!idle_mode)
+    set_millis(start + orgn);
+
+  RTC.INTCTRL &= ~RTC_CMP_bm;
+}
+
+#if 1
+unsigned int getBandgap ()
+{
+  analogReference(INTERNAL1V024);
+  analogRead(ADC_VDDDIV10); // drop first
+  return analogRead(ADC_VDDDIV10);
+} // end of getBandgap
+#endif
+
+#if GET_BATTERY
+unsigned int getBattery ()
+{
+  analogReference(VDD);
+
+  analogRead(PIN_PD1);
+  unsigned int val = analogRead(PIN_PD1);
+
+  // 3.3v regulator with voltage divider (820k/820k):
+  const float Vref = 3300;
+  const unsigned long scale = Vref*2;
+  unsigned int results = scale * val / 0x400;
+
+  return results;
+} // end of getBattery
+#endif
+
+#if 0
+unsigned int getTemp()
+{
+  analogReference(INTERNAL2V048);
+  analogRead(ADC_TEMPERATURE);
+  uint16_t adc_reading = analogRead(ADC_TEMPERATURE);
+  uint16_t sigrow_offset = SIGROW.TEMPSENSE1; // Read unsigned value from signature row
+  uint16_t sigrow_slope = SIGROW.TEMPSENSE0; // Read unsigned value from signature row
+  uint32_t temp = sigrow_offset - adc_reading;
+  temp *= sigrow_slope; // Result will overflow 16-bit variable
+  temp += 0x0800; // Add 4096/2 to get correct rounding on division below
+  temp >>= 12; // Round off to nearest degree in Kelvin, by dividing with 2^12 (4096)
+  return temp-273;
+}
+#endif
+
+void blinkN(uint8_t n, uint8_t l = LED)
+{
+  for (uint8_t i = 0; i < n; i++) {
+    digitalWriteFast(l, HIGH);
+    sleepDelay(ON_TIME);
+    digitalWriteFast(l, LOW);
+    sleepDelay(700);
+  }
+}
+
+void blinkDec(uint8_t d)
+{
+  blinkN(d / 10);
+  sleepDelay(1500);
+  blinkN(d % 10);
+  sleepDelay(3000);
+}
+
+void blinkDec3(uint16_t d)
+{
+  blinkN(d / 100);
+  sleepDelay(1500);
+  blinkDec(d % 100);
+}
+
+
+void setup() {
+  Serial.begin(38400, SERIAL_TX_ONLY);
+  Serial.println("Starting");
+  delay(100);
+
+  RTC_init();                           /* Initialize the RTC timer */
+  set_sleep_mode(SLEEP_MODE_STANDBY);
+  sleep_standby();
+  sleep_enable();                       /* Enable sleep mode, but not going to sleep yet */
+
+  pinMode(PIN_PA0, INPUT_PULLUP);
+  pinMode(PIN_PA1, INPUT_PULLUP);
+  pinMode(PIN_PA2, INPUT_PULLUP);
+  pinMode(PIN_PA3, INPUT_PULLUP);
+  pinMode(PIN_PA4, INPUT_PULLUP);
+  pinMode(PIN_PA5, INPUT_PULLUP);
+  pinMode(PIN_PA6, INPUT_PULLUP);
+  pinMode(PIN_PA7, INPUT_PULLUP);
+  
+#if GET_BATTERY
+  pinMode(PIN_PD1, INPUT);
+#else
+  pinMode(PIN_PD1, INPUT_PULLUP);
+#endif
+  pinMode(PIN_PD2, INPUT_PULLUP);
+  pinMode(PIN_PD3, INPUT_PULLUP);
+  pinMode(PIN_PD4, INPUT_PULLUP);
+  pinMode(PIN_PD5, INPUT_PULLUP);
+  pinMode(PIN_PD6, INPUT_PULLUP);
+  pinMode(PIN_PD7, INPUT_PULLUP);
+  
+  //pinMode(PIN_PC0, INPUT_PULLUP); // Serial out
+  pinMode(PIN_PC1, INPUT_PULLUP);
+  pinMode(PIN_PC2, INPUT_PULLUP);
+  pinMode(PIN_PC3, INPUT_PULLUP);
+  
+  pinMode(PIN_PF0, INPUT_PULLUP);
+  pinMode(PIN_PF1, INPUT_PULLUP);
+  
+  pinMode(LED, OUTPUT);
+  pinConfigure(COUNT_PIN, (PIN_DIR_INPUT | PIN_PULLUP | PIN_ISC_FALL));
+
+  for (byte i = 0; i < 3; i++) {
+    blinkN(1, LED);
+  }
+
+  Serial.flush();
+  Serial.println(F("\nSetup ... "));
+
+  Serial.println(F("Initialise the radio"));
+  int16_t state = radio.begin();
+  debug(state != RADIOLIB_ERR_NONE, F("Initialise radio failed"), state, true);
+
+  fillDEVEUI((uint8_t*)&devEUI);
+  // Setup the OTAA session information
+  //state = node.beginOTAA(joinEUI, devEUI, nwkKey, appKey);
+  state = node.beginOTAA(joinEUI, devEUI, NULL, appKey);
+  debug(state != RADIOLIB_ERR_NONE, F("Initialise node failed"), state, true);
+
+  Serial.println(F("Join ('login') the LoRaWAN Network"));
+  state = node.activateOTAA();
+  debug(state != RADIOLIB_LORAWAN_NEW_SESSION, F("Join failed"), state, true);
+
+  Serial.println(F("Ready!\n"));
+  //node.setSleepFunction(mdelay_long);
+}
+
+struct {
+  unsigned short temp;
+  unsigned short rain;
+  byte power;
+  byte rate2;
+} mydata;
+
+#define voltagePin    PA1
+
+void readData()
+{
+    mydata.power = getBandgap() - 100;
+    noInterrupts(); // prevent race condition with rain interrupts
+    mydata.rain = counter;
+    counter = 0;
+    interrupts();
+}
+
+void loop() {
+  Serial.println(F("Sending uplink"));
+
+  readData();
+  
+  // Perform an uplink
+  uint8_t reply[2];
+  size_t replylen;
+  int16_t state = node.sendReceive((uint8_t*)&mydata, sizeof(mydata), 1, reply, &replylen);
+  debug(state < RADIOLIB_ERR_NONE, F("Error in sendReceive"), state, false);
+
+  // Check if a downlink was received 
+  // (state 0 = no downlink, state 1/2 = downlink in window Rx1/Rx2)
+  if(state > 0) {
+    Serial.println(F("Received a downlink"));
+    if (replylen == 1) {
+      mydata.rate2= reply[0];
+      uplinkIntervalSeconds= (1 << reply[0]);
+    }
+  } else {
+    Serial.println(F("No downlink received"));
+  }
+
+  Serial.print(F("Next uplink in "));
+  Serial.print(uplinkIntervalSeconds);
+  Serial.println(F(" seconds\n"));
+  Serial.flush();
+  
+  radio.sleep();
+
+  // Wait until next uplink - observing legal & TTN FUP constraints
+  sleepDelay(uplinkIntervalSeconds * 1000UL);  // delay needs milli-seconds
+ 
+  blinkN(1);
+}
---
+++ AVR128_RadioLib/config.h
@@ -0,0 +1,161 @@
+#ifndef _RADIOLIB_EX_LORAWAN_CONFIG_H
+#define _RADIOLIB_EX_LORAWAN_CONFIG_H
+
+#define Serial Serial1
+
+#include <RadioLib.h>
+
+#define DIO1_PIN PIN_PD3
+#define DIO0_PIN PIN_PD2
+#define RESET_PIN PIN_PD5
+#define SCK_PIN PIN_PA6 // SCK for SPI1
+#define MOSI_PIN PIN_PA4 // MOSI for SPI
+#define MISO_PIN PIN_PA5  // MISO for SPI
+#define CS_PIN PIN_PD6    // CS for SPI1 === NSS
+
+// first you have to set your radio model and pin configuration
+// this is provided just as a default example
+SX1276 radio = new Module(CS_PIN, DIO0_PIN, RESET_PIN, DIO1_PIN);
+
+// if you have RadioBoards (https://github.com/radiolib-org/RadioBoards)
+// and are using one of the supported boards, you can do the following:
+/*
+#define RADIO_BOARD_AUTO
+#include <RadioBoards.h>
+
+Radio radio = new RadioModule();
+*/
+
+// how often to send an uplink - consider legal & FUP constraints - see notes
+//const uint32_t uplinkIntervalSeconds = 1UL * 60UL;    // minutes x seconds
+
+#include "secrets.h"
+
+// joinEUI - previous versions of LoRaWAN called this AppEUI
+// for development purposes you can use all zeros - see wiki for details
+#ifndef RADIOLIB_LORAWAN_JOIN_EUI
+#define RADIOLIB_LORAWAN_JOIN_EUI  REPLACE
+#endif
+
+// the Device EUI & two keys can be generated on the TTN console 
+#ifndef RADIOLIB_LORAWAN_DEV_EUI   // Replace with your Device EUI
+#define RADIOLIB_LORAWAN_DEV_EUI   0
+#endif
+#ifndef RADIOLIB_LORAWAN_APP_KEY   // Replace with your App Key 
+#define RADIOLIB_LORAWAN_APP_KEY   REPLACE
+#endif
+#ifndef RADIOLIB_LORAWAN_NWK_KEY   // Put your Nwk Key here
+#define RADIOLIB_LORAWAN_NWK_KEY   NULL
+#endif
+
+// for the curious, the #ifndef blocks allow for automated testing &/or you can
+// put your EUI & keys in to your platformio.ini - see wiki for more tips
+
+// regional choices: EU868, US915, AU915, AS923, AS923_2, AS923_3, AS923_4, IN865, KR920, CN470
+const LoRaWANBand_t Region = EU868;
+
+// subband choice: for US915/AU915 set to 2, for CN470 set to 1, otherwise leave on 0
+const uint8_t subBand = 0;
+
+// ============================================================================
+// Below is to support the sketch - only make changes if the notes say so ...
+
+// copy over the EUI's & keys in to the something that will not compile if incorrectly formatted
+uint64_t joinEUI =   RADIOLIB_LORAWAN_JOIN_EUI;
+uint64_t devEUI  =   RADIOLIB_LORAWAN_DEV_EUI;
+uint8_t appKey[] = { RADIOLIB_LORAWAN_APP_KEY };
+uint8_t nwkKey[] = { RADIOLIB_LORAWAN_NWK_KEY };
+
+// create the LoRaWAN node
+LoRaWANNode node(&radio, &Region, subBand);
+
+// result code to text - these are error codes that can be raised when using LoRaWAN
+// however, RadioLib has many more - see https://jgromes.github.io/RadioLib/group__status__codes.html for a complete list
+String stateDecode(const int16_t result) {
+  switch (result) {
+  case RADIOLIB_ERR_NONE:
+    return "ERR_NONE";
+  case RADIOLIB_ERR_CHIP_NOT_FOUND:
+    return "ERR_CHIP_NOT_FOUND";
+  case RADIOLIB_ERR_PACKET_TOO_LONG:
+    return "ERR_PACKET_TOO_LONG";
+  case RADIOLIB_ERR_RX_TIMEOUT:
+    return "ERR_RX_TIMEOUT";
+  case RADIOLIB_ERR_CRC_MISMATCH:
+    return "ERR_CRC_MISMATCH";
+  case RADIOLIB_ERR_INVALID_BANDWIDTH:
+    return "ERR_INVALID_BANDWIDTH";
+  case RADIOLIB_ERR_INVALID_SPREADING_FACTOR:
+    return "ERR_INVALID_SPREADING_FACTOR";
+  case RADIOLIB_ERR_INVALID_CODING_RATE:
+    return "ERR_INVALID_CODING_RATE";
+  case RADIOLIB_ERR_INVALID_FREQUENCY:
+    return "ERR_INVALID_FREQUENCY";
+  case RADIOLIB_ERR_INVALID_OUTPUT_POWER:
+    return "ERR_INVALID_OUTPUT_POWER";
+  case RADIOLIB_ERR_NETWORK_NOT_JOINED:
+	  return "RADIOLIB_ERR_NETWORK_NOT_JOINED";
+  case RADIOLIB_ERR_DOWNLINK_MALFORMED:
+    return "RADIOLIB_ERR_DOWNLINK_MALFORMED";
+  case RADIOLIB_ERR_INVALID_REVISION:
+    return "RADIOLIB_ERR_INVALID_REVISION";
+  case RADIOLIB_ERR_INVALID_PORT:
+    return "RADIOLIB_ERR_INVALID_PORT";
+  case RADIOLIB_ERR_NO_RX_WINDOW:
+    return "RADIOLIB_ERR_NO_RX_WINDOW";
+  case RADIOLIB_ERR_INVALID_CID:
+    return "RADIOLIB_ERR_INVALID_CID";
+  case RADIOLIB_ERR_UPLINK_UNAVAILABLE:
+    return "RADIOLIB_ERR_UPLINK_UNAVAILABLE";
+  case RADIOLIB_ERR_COMMAND_QUEUE_FULL:
+    return "RADIOLIB_ERR_COMMAND_QUEUE_FULL";
+  case RADIOLIB_ERR_COMMAND_QUEUE_ITEM_NOT_FOUND:
+    return "RADIOLIB_ERR_COMMAND_QUEUE_ITEM_NOT_FOUND";
+  case RADIOLIB_ERR_JOIN_NONCE_INVALID:
+    return "RADIOLIB_ERR_JOIN_NONCE_INVALID";
+  case RADIOLIB_ERR_N_FCNT_DOWN_INVALID:
+    return "RADIOLIB_ERR_N_FCNT_DOWN_INVALID";
+  case RADIOLIB_ERR_A_FCNT_DOWN_INVALID:
+    return "RADIOLIB_ERR_A_FCNT_DOWN_INVALID";
+  case RADIOLIB_ERR_DWELL_TIME_EXCEEDED:
+    return "RADIOLIB_ERR_DWELL_TIME_EXCEEDED";
+  case RADIOLIB_ERR_CHECKSUM_MISMATCH:
+    return "RADIOLIB_ERR_CHECKSUM_MISMATCH";
+  case RADIOLIB_ERR_NO_JOIN_ACCEPT:
+    return "RADIOLIB_ERR_NO_JOIN_ACCEPT";
+  case RADIOLIB_LORAWAN_SESSION_RESTORED:
+    return "RADIOLIB_LORAWAN_SESSION_RESTORED";
+  case RADIOLIB_LORAWAN_NEW_SESSION:
+    return "RADIOLIB_LORAWAN_NEW_SESSION";
+  case RADIOLIB_ERR_NONCES_DISCARDED:
+    return "RADIOLIB_ERR_NONCES_DISCARDED";
+  case RADIOLIB_ERR_SESSION_DISCARDED:
+    return "RADIOLIB_ERR_SESSION_DISCARDED";
+  }
+  return "See https://jgromes.github.io/RadioLib/group__status__codes.html";
+}
+
+// helper function to display any issues
+void debug(bool failed, const __FlashStringHelper* message, int state, bool halt) {
+  if(failed) {
+    Serial.print(message);
+    Serial.print(" - ");
+    Serial.print(stateDecode(state));
+    Serial.print(" (");
+    Serial.print(state);
+    Serial.println(")");
+    while(halt) { delay(1); }
+  }
+}
+
+// helper function to display a byte array
+void arrayDump(uint8_t *buffer, uint16_t len) {
+  for(uint16_t c = 0; c < len; c++) {
+    char b = buffer[c];
+    if(b < 0x10) { Serial.print('0'); }
+    Serial.print(b, HEX);
+  }
+  Serial.println();
+}
+
+#endif
